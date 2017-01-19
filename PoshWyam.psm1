600bbfaf1521d1ce7a92fc21e5d2aa421d833730
Bill Kempf <wekempf@outlook.com>
2017-01-19 15:44:30 -0500

    Add module manifest and module script.

---
+++ PoshWyam.psm1
@@ -0,0 +1,13 @@
+$ModuleRoot = $PSScriptRoot
+
+# Public cmdlets
+#. $ModuleRoot\Get-BlogPost.ps1
+#. $ModuleRoot\Get-BlogPostsLocation.ps1
+#. $ModuleRoot\Get-WyamRoot.ps1
+#. $ModuleRoot\New-Blog.ps1
+#. $ModuleRoot\New-BlogPost.ps1
+#. $ModuleRoot\Set-BlogPostPublishedDate.ps1
+
+# Private cmdlets
+#. $ModuleRoot\Get-BlogPostName.ps1
+#. $ModuleRoot\Join-Paths.ps1
