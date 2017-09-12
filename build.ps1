4b7ebfd197ba2bc4966bc5c0f34d53f9ab86b130
Bill Kempf <wekempf@outlook.com>
2017-09-12 09:31:54 -0400

    WIP

--- build.ps1
+++ build.ps1
@@ -1,18 +1,20 @@
 [CmdletBinding()]
 Param(
     [Parameter(Position=0, Mandatory=$False)]
-    [ValidateSet("Clean", "Build", "Test", "Import")]
+    [ValidateSet("Clean", "Build", "Test", "Install")]
     [string[]]$TaskList,
-
+    [string]$InstallDir,
     [switch]$Develop
 )
+
 Push-Location $PSScriptRoot
 try {
     if (-not (Test-Path 'tools')) {
         [void](New-Item -Name 'tools' -ItemType Directory)
     }
+
     $psake = Get-ChildItem -Include psake.psd1 -Path 'tools' -Recurse
-    if (-not (Test-Path $psake)) {
+    if ((-not $psake) -or (-not (Test-Path $psake))) {
         Save-Module -Name Psake -Path 'tools'
         $psake = Get-ChildItem -Include psake.psd1 -Path 'tools' -Recurse
         if (-not (Test-Path $psake)) {
@@ -20,15 +22,11 @@ try {
         }
     }
     Import-Module $psake -Force
-    $params = $PSBoundParameters + @{ 'BuildFile' = 'poshwyam.psake.ps1'; 'ErrorAction' = 'Stop'; 'Parameters' = @{ 'dev' = "$Develop" } }
-    $params.Remove('Develop')
-    Invoke-Psake @params
 
-    $poshWyam = "$(Get-Location)\output\PoshWyam\PoshWyam.psd1"
-    if ($Develop -and (Test-Path $poshWyam)) {
-        Write-Host "Importing $poshWyam."
-        Import-Module $poshWyam -Force
-    }
+    $params = $PSBoundParameters + @{ 'BuildFile' = 'poshwyam.psake.ps1'; 'ErrorAction' = 'Stop'; 'Parameters' = @{ 'dev' = "$Develop"; 'InstallDir' = "$InstallDir" } }
+    $params.Remove('Develop')
+    $params.Remove('InstallDir')
+    Invoke-Psake @params
 } finally {
     Pop-Location
 }
--- poshwyam.psake.ps1
+++ poshwyam.psake.ps1
@@ -13,10 +13,10 @@ Task default -Depends Test
 Task Clean {
     if ($Develop) {
         Write-Host "Cleaning '.\output\PoshWyam\'."
-        Get-ChildItem -Path 'output/PoshWyam' -Exclude 'Wyam','lib' | Remove-Item -Recurse -Force
+        Get-ChildItem -Path 'output/PoshWyam' -Exclude 'Wyam','lib' | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
     } else {
         Write-Host "Cleaning '.\output\'."
-        Get-ChildItem -Path 'output' | Remove-Item -Recurse -Force
+        Get-ChildItem -Path 'output' | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
     }
 }
 
@@ -53,7 +53,19 @@ Task Test -Depends Build {
 }
 
 Task Import -Depends Build {
+    Write-Host "Importing module"
     import-module '.\output\PoshWyam\PoshWyam.psd1' -Force
+}
+
+Task Install -Depends Build {
+    Write-Host "Installing to $InstallDir"
+    if ((-not $InstallDir) -or (-not (Test-Path $InstallDir))) {
+        throw "InstallDir unspecified or destination does not exist"
+    }
+    if (Test-Path "$InstallDir/PoshWhyam") {
+        Remove-Item -Path "$InstallDir/PoshWyam" -Recurse -Force
+    }
+    Copy-Item -Path "$PSScriptRoot/output/PoshWyam/*" -Destination "$InstallDir/PoshWyam" -Force -Recurse
 }
 
 function EnsureModule {
@@ -65,11 +77,14 @@ function EnsureModule {
     Write-Host "Finding $Name."
     $module = Get-Module -Name $Name
     if (-not $module) {
-        $module = Get-ChildItem -Include "$Name.psd1" -Path "output" -Recurse | Select-Object -First 1
+        $module = Get-ChildItem -Include "$Name.psd1" -Path "$PSScriptRoot/output" -Recurse | Select-Object -First 1
         if (-not $module) {
             Write-Host "Installing $Name."
-            Save-Module -Name $Name -Path "output"
-            $module = Get-ChildItem -Include "$Name.psd1" -Path "output" -Recurse | Select-Object -First 1
+            Save-Module -Name $Name -Path "output" -ErrorAction SilentlyContinue
+            $module = Get-ChildItem -Include "$Name.psd1" -Path "$PSScriptRoot/output" -Recurse | Select-Object -First 1
+            if (-not $module) {
+                throw "Unable to bootstrap $Name."
+            }
         }
         Write-Host "Importing $Name."
         Import-Module $module
@@ -89,14 +104,13 @@ function EnsureNuGet {
         $Destination = "output"
     }
 
-    Write-Host "$Develop"
     Write-Host "Finding NuGet.exe."
-    $nuget = Join-Path 'tools' 'nuget.exe'
+    $nuget = Join-Path (Join-Path $PSScriptRoot 'tools') 'nuget.exe'
     if (-not (Test-Path $nuget)) {
         $nuget = (Get-Command -Name 'nuget.exe' -ErrorAction SilentlyContinue).Source
         if (-not $nuget) {
             Write-Host "Installing NuGet.exe"
-            $nuget = Join-Path 'tools' 'nuget.exe'
+            $nuget = Join-Path (Join-Path $PSScriptRoot 'tools') 'nuget.exe'
             try {
                 (New-Object System.Net.WebClient).DownloadFile("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe", $nuget)
             } catch {
